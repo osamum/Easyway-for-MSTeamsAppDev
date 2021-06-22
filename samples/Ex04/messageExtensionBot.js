854d28beeede7510997f7a3f1e7deae53c14b4b6
osamum <osamum@microsoft.com>
2021-06-22 13:17:49 +0900

    Update Messaging Extension handson

---
+++ samples/Ex04/messageExtensionBot.js
@@ -0,0 +1,163 @@
+const axios = require("axios");
+const querystring = require("querystring");
+const { TeamsActivityHandler, CardFactory } = require("botbuilder");
+
+class MessageExtensionBot extends TeamsActivityHandler {
+  // Action.
+  handleTeamsMessagingExtensionSubmitAction(context, action) {
+    switch (action.commandId) {
+      case "createCard":
+        return createCardCommand(context, action);
+      case "shareMessage":
+        return shareMessageCommand(context, action);
+      default:
+        throw new Error("NotImplemented");
+    }
+  }
+
+  // Search.
+  async handleTeamsMessagingExtensionQuery(context, query) {
+    const searchQuery = query.parameters[0].value;
+    /*
+    const response = await axios.get(
+      `http://registry.npmjs.com/-/v1/search?${querystring.stringify({
+        text: searchQuery,
+        size: 8,
+      })}`
+    );
+    */
+    //Wikipedia の API のエンドポイントを指定
+    const response = await axios.get(`https://ja.wikipedia.org/w/rest.php/v1/search/page?${querystring.stringify({ q: searchQuery, size: 8 })}&limit=10`);
+
+    const attachments = [];
+    /*
+    response.data.objects.forEach((obj) => {
+      const heroCard = CardFactory.heroCard(obj.package.name);
+      const preview = CardFactory.heroCard(obj.package.name);
+      preview.content.tap = {
+        type: "invoke",
+        value: { description: obj.package.description },
+      };
+      const attachment = { ...heroCard, preview };
+      attachments.push(attachment);
+    });
+    */
+
+      //Wikipedia API が返すデータの構造に合わせた変更
+       response.data.pages.forEach(obj => {
+            const heroCard = CardFactory.heroCard(obj.title);
+            const preview = CardFactory.heroCard(obj.title); // Preview cards are optional for Hero card. You need them for Adaptive Cards.
+            preview.content.tap = { type: 'invoke', value: { description: obj.excerpt } };
+            const attachment = { ...heroCard, preview };
+            attachments.push(attachment);
+        });
+
+    return {
+      composeExtension: {
+        type: "result",
+        attachmentLayout: "list",
+        attachments: attachments,
+      },
+    };
+  }
+
+  async handleTeamsMessagingExtensionSelectItem(context, obj) {
+    return {
+      composeExtension: {
+        type: "result",
+        attachmentLayout: "list",
+        attachments: [CardFactory.thumbnailCard(obj.description)],
+      },
+    };
+  }
+
+  // Link Unfurling.
+  handleTeamsAppBasedLinkQuery(context, query) {
+    const attachment = CardFactory.thumbnailCard("Thumbnail Card", query.url, [query.url]);
+
+    const result = {
+      attachmentLayout: "list",
+      type: "result",
+      attachments: [attachment],
+    };
+
+    const response = {
+      composeExtension: result,
+    };
+    return response;
+  }
+}
+
+function createCardCommand(context, action) {
+  // The user has chosen to create a card by choosing the 'Create Card' context menu command.
+  const data = action.data;
+  const heroCard = CardFactory.heroCard(data.title, data.text);
+  heroCard.content.subtitle = data.subTitle;
+  const attachment = {
+    contentType: heroCard.contentType,
+    content: heroCard.content,
+    preview: heroCard,
+  };
+
+  return {
+    composeExtension: {
+      type: "result",
+      attachmentLayout: "list",
+      attachments: [attachment],
+    },
+  };
+}
+
+function shareMessageCommand(context, action) {
+  // The user has chosen to share a message by choosing the 'Share Message' context menu command.
+  let userName = "unknown";
+  if (
+    action.messagePayload &&
+    action.messagePayload.from &&
+    action.messagePayload.from.user &&
+    action.messagePayload.from.user.displayName
+  ) {
+    userName = action.messagePayload.from.user.displayName;
+  }
+
+  // This Messaging Extension example allows the user to check a box to include an image with the
+  // shared message.  This demonstrates sending custom parameters along with the message payload.
+  let images = [];
+  const includeImage = action.data.includeImage;
+  if (includeImage === "true") {
+    images = [
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQtB3AwMUeNoq4gUBGe6Ocj8kyh3bXa9ZbV7u1fVKQoyKFHdkqU",
+    ];
+  }
+  const heroCard = CardFactory.heroCard(
+    `${userName} originally sent this message:`,
+    action.messagePayload.body.content,
+    images
+  );
+
+  if (
+    action.messagePayload &&
+    action.messagePayload.attachment &&
+    action.messagePayload.attachments.length > 0
+  ) {
+    // This sample does not add the MessagePayload Attachments.  This is left as an
+    // exercise for the user.
+    heroCard.content.subtitle = `(${action.messagePayload.attachments.length} Attachments not included)`;
+  }
+
+  const attachment = {
+    contentType: heroCard.contentType,
+    content: heroCard.content,
+    preview: heroCard,
+  };
+
+  return {
+    composeExtension: {
+      type: "result",
+      attachmentLayout: "list",
+      attachments: [attachment],
+    },
+  };
+}
+
+module.exports.MessageExtensionBot = MessageExtensionBot;
